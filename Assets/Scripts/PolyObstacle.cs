6cc99eed89c27e04fd4491ce5975f4ea038ecb1d
Maria <MariaIvanuss@gmail.com>
2018-02-01 20:05:58 +0200

    Ver 28.01.18
    Final version from Global Game Jam Ukraine 2018

---
+++ Assets/Scripts/PolyObstacle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PolyObstacle : MonoBehaviour {
+
+	public bool initializeOnStart = false;
+	public Transform[] choices;
+
+
+	void Start() {
+		if (initializeOnStart)
+			InitializeObstacle();
+	}
+
+
+	public void InitializeObstacle() {
+		int index = 0;
+
+		if (choices != null) {
+			if (choices.Length > 1)
+				index = Random.Range(0, choices.Length);
+		}
+		
+		Transform obj = choices[index];
+
+		Instantiate(obj, transform.position, obj.rotation);
+	}
+
+	
+	void OnDrawGizmos() {
+		
+		Gizmos.color = Color.gray;
+
+		Gizmos.DrawWireSphere(transform.position, 0.5f);
+	}
+
+}
